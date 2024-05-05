08f3a3d265fdc3535cbb070af98fac82f421f029
Felipe <felipematheusgr18@gmail.com>
2024-05-05 15:15:19 -0300

    inserção das consultas no DB

--- ProjetoFilmesDIO/Consultas de filmes.sql
+++ ProjetoFilmesDIO/Consultas de filmes.sql
@@ -6,3 +6,37 @@ ORDER BY Ano
 SELECT Nome, Ano, Duracao FROM Filmes
 WHERE Nome = 'De volta para o futuro'
 
+SELECT * FROM Filmes
+WHERE Ano = 1997
+
+SELECT * FROM Filmes
+WHERE Ano > 2000
+
+SELECT * FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+SELECT Ano,
+COUNT (*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+SELECT * FROM Atores
+WHERE Genero = 'M'
+
+SELECT * FROM Atores
+WHERE Genero = 'F'
+
+SELECT Nome, Genero FROM Filmes
+INNER JOIN Generos ON Filmes.Id = Generos.Id
+
+SELECT Nome, Genero FROM Filmes
+INNER JOIN Generos ON Filmes.Id = Generos.Id
+WHERE Generos.Genero = 'Mistério'
+
+
+--SELECT * FROM Filmes
+--SELECT * FROM Generos
+--SELECT * FROM FilmesGenero
+
